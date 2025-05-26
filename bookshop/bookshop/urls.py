fa3df1e6772af6369202e25337d4ee54c0cea480
Khadeim <khadbury@gmail.com>
2025-05-26 21:58:55 +0100

    Implemented a login form and page with a base html styling page

--- bookshop/bookshop/urls.py
+++ bookshop/bookshop/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +24,8 @@ urlpatterns = [
     path('', include('menu.urls')),
     path('', include('books.urls')),
     path('', include('orders.urls')),
+
+    #Login and Logout routes
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
